9442b8e15d0c1f5bb5d2fea8f393d1af4aec2725
BDS27 <brandon.santacruz@epn.edu.ec>
2025-05-20 16:48:35 -0500

    remove keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,6 @@
 export const environment = {
   production: false,
   firebase: {
-
-    apiKey: "AIzaSyAXF_1CvR9ls9Bcmx_8H4lLuPkZ_sqdXmk",
-    authDomain: "appmovi-4a4be.firebaseapp.com",
-    projectId: "appmovi-4a4be",
-    storageBucket: "appmovi-4a4be.firebasestorage.app",
-    messagingSenderId: "626324811593",
-    appId: "1:626324811593:web:b1ffc1d17c9ae714a6f498",
-    measurementId: "G-T0CP8H2JZD"
-
   }
 };
 
